736c7c852a98dd3f22ba236226ed477328519012
Enrique R <enriquevr@gmail.com>
2014-09-25 15:31:05 -0500

    testing implementation

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,161 +1,197 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-	grunt.initConfig({  
-		copy: {
-		    main: {
+    grunt.initConfig({
+        copy: {
+            resources: {
+                cwd: 'app',
+                src: ['html/*.html', 'index.html', 'css/fonts/*'],
+                dest: 'dist/',
+                expand: true
+            },
+
+            dependencies: {
                 cwd: 'app/',
-		        src: ['js/**','html/*','index.html', 'css/main.css', 'css/fonts/*'],
-		        dest: 'dist/',
-		        expand: true
-		    }
-		},
-		scp: {
-		    options: {
-		        host: '127.0.0.1',
-		        username: 'root',
-		        password: 'hadoop',
-		        port: 2222
-		    },
-		    sandbox: {
-		        files: [{
-					cwd: 'dist',
-		            src: '**',
-		            filter: 'isFile',
-		           
-		            // path on the server
-		            dest: '/var/lib/falcon/webapp/falcon'
-		        }]
-		    }
-		},  
+                src: ['js/lib/*.js', 'css/fonts/*'],
+                dest: 'dist/',
+                expand: true
+            }
+        },
+
         uglify: {
             options: {
                 beautify: false,
                 mangle: true,
-                compress: true, 
+                compress: true,
                 preserveComments: false,
                 drop_console: false,
-                sourceMap: "dist/application.map",
-                banner: "/**** Created by Santiago Rebella ***/"
+                sourceMap: 'dist/application.map',
+                banner: '/**** Created by Santiago Rebella ***/'
             },
-            main: {                             
-                src: "js/main.js",              
-                dest: "dist/js/main.min.js"     
-            }                                   
+            main: {
+                files: {
+                    'dist/js/main.min.js': [
+                        'app/js/app.js',
+                        'app/js/controllers/*.js',
+                        'app/js/directives/*.js',
+                        'app/js/services/*.js'
+                    ]
+                }
+            }
         },
+
         jshint: {
             options: {
                 eqeqeq: true,
                 curly: true,
                 undef: false,
-                unused: true
+                unused: true,
+                force: true
             },
             target: {
-                src: [ 
-                    "*.js",
-	                "app/js/*.js", 
-	                "app/js/**/*.js",
-	                "!app/js/lib/*.js"
+                src: [
+                    'app/js/app.js',
+                    'app/js/controllers/*.js',
+                    'app/js/directives/*.js',
+                    'app/js/services/*.js'        ]
+            }
+        },
+
+        csslint: {
+            strict: {
+                src: ['dist/css/*.css']
+            }
+        },
+
+        datauri: {
+            'default': {
+                options: {
+                    classPrefix: 'data-'
+                },
+                src: [
+                    'css/img/*.png',
+                    'css/img/*.gif',
+                    'css/img/*.jpg',
+                    'css/img/*.bmp'
+                ],
+                dest: [
+                    'tmp/base64Images.css'
                 ]
             }
-        },       
-        csslint: {
-            strict: {                    
-                src: ["public/css/*.css"]
-            }
         },
-        
-        datauri: {
-		    "default": {
-		        options: {
-		            classPrefix: "data-"
-		        },
-		        src: [
-		            "css/img/*.png",
-		            "css/img/*.gif",
-		            "css/img/*.jpg",
-		            "css/img/*.bmp"
-		        ],
-		        dest: [
-		            "tmp/base64Images.css"
-		        ]
-		    }
-        },
+
         less: {
             development: {
                 options: {
                     compress: true,
                     yuicompress: false,
                     optimization: 2,
-                    cleancss:false,    
+                    cleancss: false,
                     syncImport: false,
-                    strictUnits:false,
+                    strictUnits: false,
                     strictMath: true,
                     strictImports: true,
-                    ieCompat: false    
+                    ieCompat: false
                 },
                 files: {
-                    "app/css/main.css": "app/css/main.less"
+                    'dist/css/main.css': 'app/css/main.less'
                 }
             }
         },
+
         watch: {
-            livereload: {
-              options: { livereload: true },
-              files: [
-                  "app/css/main.css", 
-                  "app/html/*.html", 
-                  "app/js/*.js", 
-                  "app/js/**/*.js"
-              ]
-            },
-   //         scripts: {
-   //             files: [
-   //                 "app/js/*.js",
-   //                 "app/js/**/*.js"
-   //             ],
-   //             tasks: ["jshint"]
-   //         },
+
             less: {
-                files: ["app/css/*.less", "app/css/less/*.less"],
-                tasks: ["less", "csslint"], 
+                files: ['app/css/*.less', 'app/css/less/*.less'],
+                tasks: ['less', 'csslint'],
                 options: {
                     nospawn: true
                 }
-            }/*,
-            express: {
-				files:  [ 'server.js' ],
-				tasks:  [ 'express:dev' ],
-				options: {
-					spawn: false
-					
-				}
-			}*/
+            },
+
+            resources: {
+                options: {
+                    livereload: true
+                },
+                files: ['app/html/*', 'app/index.html', 'app/css/fonts/*'],
+                tasks: ['resources']
+            },
+
+            source: {
+                options: {
+                    livereload: true
+                },
+                files: ['app/js/**/*.js'],
+                tasks: ['jshint', 'karma:unit:run']
+            }
         },
+
         express: {
-			server: {
-				options: {
-					script: 'server.js'
-				}
-			}
-			
-		}
+            server: {
+                options: {
+                    script: 'server.js'
+                }
+            }
+        },
+
+        clean: ["dist"],
+
+        scp: {
+            options: {
+                host: '127.0.0.1',
+                username: 'root',
+                password: 'hadoop',
+                port: 2222
+            },
+
+            sandbox: {
+                files: [{
+                    cwd: 'dist',
+                    src: '**',
+                    filter: 'isFile',
+
+                    // path on the server
+                    dest: '/var/lib/falcon/webapp/falcon'
+                }]
+            }
+        },
+
+        karma: {
+            unit: {
+                configFile: 'karma.conf.js',
+                background: true,
+                singleRun: false,
+                autoWatch: false
+            },
+
+            continuous: {
+                configFile: 'karma.conf.js',
+                singleRun: true,
+                autoWatch: false,
+                browsers: ['PhantomJS']
+            }
+        }
+
     });
 
-    grunt.registerTask("default", ["copy", "express","watch"]); // van en orden primero el primero
-	
-	grunt.registerTask("deploy", ["copy","scp"]);
-	
-	// grunt.registerTask("livedeploy", ["copy","scp"]); --- i need more time on this to configure it to listen changes and deploy auto
-	
-    grunt.registerTask("data64", ["datauri"]);
+    grunt.registerTask('resources', ['copy:resources']);
+    grunt.registerTask('dependencies', ['copy:dependencies']);
+    grunt.registerTask('test', ['karma:continuous']);
+    grunt.registerTask('build', ['clean', 'uglify', 'less', 'resources', 'dependencies']);
+    grunt.registerTask('w', ['build', 'karma:unit:start', 'watch']);
+    grunt.registerTask('server', ['express','w']);
+    grunt.registerTask('default', ['server']);
+    grunt.registerTask('data64', ['datauri']);
 
-    grunt.loadNpmTasks("grunt-contrib-uglify");
-    grunt.loadNpmTasks("grunt-contrib-jshint");
-    grunt.loadNpmTasks("grunt-contrib-watch");
-    grunt.loadNpmTasks("grunt-contrib-less");
-    grunt.loadNpmTasks("grunt-contrib-csslint");    
-    grunt.loadNpmTasks("grunt-contrib-copy");  
-    grunt.loadNpmTasks('grunt-scp'); 
-    grunt.loadNpmTasks('grunt-express-server'); 
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-csslint');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-scp');
+    grunt.loadNpmTasks('grunt-express-server');
     grunt.loadNpmTasks('grunt-datauri');
+    grunt.loadNpmTasks('grunt-karma');
+
 };
--- server.js
+++ server.js
@@ -1,16 +1,12 @@
 (function () {
-	"use strict";
-	
-    var express = require('express'),
-        appDev = express(),
-        appProd = express();
-    
-    appDev.use(express.static(__dirname+'/app'));
-    appDev.listen(3000);
-    
-	appProd.use(express.static(__dirname+'/dist'));
-    appProd.listen(3100);
-    
-    console.log('Prod server listening on port 3100 \n Dev server listening on port 3000');
-	 
+    "use strict";
+
+    var express = require('express');
+    var app = express();
+
+    app.use(express.static(__dirname + '/dist'));
+    app.listen(3000);
+
+    console.log('Dev server listening on port 3000');
+
 })();
