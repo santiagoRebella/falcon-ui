790912369cbc9463933a12b6ae5df72d25af6a2a
Enrique Ramos <enriquevr@gmail.com>
2014-10-07 22:51:00 -0500

    splitting into files, dates

--- app/js/services/entity-factory.js
+++ app/js/services/entity-factory.js
@@ -17,9 +17,9 @@
  */
 (function () {
   'use strict';
-  var module = angular.module('app.services.entity.factory', []);
+  var module = angular.module('app.services.entity.factory', ['app.services.entity.transformer']);
 
-  module.factory('EntityFactory', function() {
+  module.factory('EntityFactory', ['EntityTransformerFactory', function(EntityTransformerFactory) {
     return {
       newFeed: function() {
         return new Feed();
@@ -27,11 +27,14 @@
 
       newCluster: function(type, selected) {
         return new Cluster(type, selected);
+      },
+
+      transform: function(feed) {
+        return transformFeed(feed, EntityTransformerFactory);
       }
 
     };
-  });
-
+  }]);
 
   function Feed() {
     this.name = null;
@@ -132,9 +135,115 @@
   }
 
   function DateAndTime() {
-    this.date = '2014-02-28';
+    this.date = new Date();
     this.time = '00:00';
+    this.opened = false;
   }
 
+  function keyValuePairs(input) {
+    return input.filter(emptyKey).map(entryToString).join(',');
+  }
+
+  function emptyKey (input) {
+    return input.key;
+  }
+
+  function emptyValue (input) {
+    return input && input.value;
+  }
+
+  function emptyFrequency (input) {
+    return input.value.unit ? input.value.quantity : input.value;
+  }
+
+  function entryToString(input) {
+    return input.key + '=' + input.value;
+  }
+
+  function frequencyToString(input) {
+    return input.quantity ? input.unit + '(' + input.quantity + ')' : null;
+  }
+
+  function timeAndDateToString(input) {
+    return input.date + 'T'  + input.time + 'Z';
+  }
+
+  function emptyElement() {return {};}
+
+  function FeedModel() {
+    this.feed = {_xmlns: "uri:falcon:feed:0.1"};
+  }
+
+  function transformFeed (feed, transformerFactory) {
+    var propertyTransform = transformerFactory
+      .transform('key', '_name')
+      .transform('value', '_value', function(value) {
+        return value.quantity ? frequencyToString(value) : value;
+      });
+
+    var locationTransform = transformerFactory
+      .transform('type', '_type')
+      .transform('path', '_path');
+
+    var clusterTransform = transformerFactory
+      .transform('name', '_name')
+      .transform('type', '_type')
+      .transform('validity.start', 'validity._start', timeAndDateToString)
+      .transform('validity.end', 'validity._end', timeAndDateToString)
+      .transform('retention', 'retention._limit', frequencyToString)
+      .transform('retention.action', 'retention._action')
+      .transform('storage.fileSystem', 'locations.location', function(fileSystem) {
+        return feed.storage.fileSystem.active ? transformfileSystem(fileSystem) : null;
+      })
+      .transform('storage.catalog', 'table', function(catalog) {
+        return feed.storage.catalog.active ? transformCatalog(catalog) : null;
+      });
+
+    var transform = transformerFactory
+      .transform('name', 'feed._name')
+      .transform('description', 'feed._description')
+      .transform('tags', 'feed.tags', keyValuePairs)
+      .transform('groups', 'feed.groups')
+      .transform('availabilityFlag', 'feed.availabilityFlag')
+      .transform('frequency', 'feed.frequency', frequencyToString)
+      .transform('timezone', 'feed.timezone')
+      .transform('lateArrival.cutOff', 'feed.late-arrival._cut-off', frequencyToString)
+      .transform('clusters', 'feed.clusters.cluster', function(clusters) {
+        return clusters.map(function(cluster) {
+          return clusterTransform.apply(cluster, {});
+        });
+      })
+      .transform('storage.fileSystem', 'feed.locations.location', function(fileSystem) {
+        return fileSystem.active ? transformfileSystem(fileSystem) : null;
+      })
+      .transform('storage.catalog', 'feed.table', function(catalog) {
+        return catalog.active ? transformCatalog(catalog) : null;
+      })
+      .transform('ACL', 'feed.ACL', emptyElement)
+      .transform('ACL.owner', 'feed.ACL._owner')
+      .transform('ACL.group', 'feed.ACL._group')
+      .transform('ACL.permission', 'feed.ACL._permission')
+      .transform('schema', 'feed.schema', emptyElement)
+      .transform('schema.location', 'feed.schema._location')
+      .transform('schema.provider', 'feed.schema._provider')
+      .transform('allproperties', 'feed.properties.property', function(properties) {
+        return properties.filter(emptyValue).filter(emptyFrequency).map(function(property) {
+          return propertyTransform.apply(property, {});
+        });
+      });
+
+    function transformfileSystem (fileSystem) {
+      return fileSystem.locations.map(function(location) {
+        return locationTransform.apply(location, {});
+      });
+    }
+
+    function transformCatalog(catalog) {
+      return {_uri : catalog.catalogTable.uri};
+    }
+
+    return transform.apply(feed, new FeedModel());
+
+  }
 
 })();
